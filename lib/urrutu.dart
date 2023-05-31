44901e26a548beb70e57a0a318e4dc9e836cce92
BanuPrasath <banuprasath0339@gmail.com>
2023-05-31 18:32:45 +0530

    Changed to Coin Flipper

--- lib/urrutu.dart
+++ lib/urrutu.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+final randomizer = Random();
+String flipval1 = "Therla";
+
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
   @override
@@ -11,10 +14,26 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var currentDiceRoll = 2;
+  var currentDiceRoll = 1;
+
+  void runer() {
+    /*var dicerolluhh = Random().nextInt(6) + 1;
+              print(dicerolluhh);*/
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(2) + 1;
+      print(currentDiceRoll);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (currentDiceRoll == 1) {
+      print("True");
+      flipval1 = "Heads";
+      print(flipval1);
+    } else {
+      flipval1 = "Tail!..";
+    }
     // TODO: implement build
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -23,16 +42,17 @@ class _DiceRollerState extends State<DiceRoller> {
           'asserts/dice-$currentDiceRoll.png',
           width: 200,
         ),
-        TextButton(
-            onPressed: () {
-              var dicerolluhh = Random().nextInt(6) + 1;
-              print(dicerolluhh);
-              setState(() {
-                currentDiceRoll = Random().nextInt(6) + 1;
-              });
-            },
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.only(top: 20),
+        SizedBox(
+            width: 70,
+            height: 50,
+            child: Text(
+              flipval1,
+              style: TextStyle(fontSize: 24, color: Colors.red),
+            )),
+        ElevatedButton(
+            onPressed: runer,
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.all(5),
               foregroundColor: Colors.white,
               textStyle: const TextStyle(fontSize: 28),
             ),
